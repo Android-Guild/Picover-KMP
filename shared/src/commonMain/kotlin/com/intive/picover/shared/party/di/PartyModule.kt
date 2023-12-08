682a9c2239c9a86956ce9c79e131fa2b1fd642ae
Marek Macko <marek.macko@intive.com>
2023-12-08 12:35:47 +0100

    Move PartiesScreen with view model to shared

--- shared/src/commonMain/kotlin/com/intive/picover/shared/party/di/PartyModule.kt
+++ shared/src/commonMain/kotlin/com/intive/picover/shared/party/di/PartyModule.kt
@@ -1,8 +1,10 @@
 package com.intive.picover.shared.party.di
 
+import com.intive.picover.parties.viewmodel.PartiesViewModel
 import com.intive.picover.parties.viewmodel.PartyDetailsViewModel
 import org.koin.dsl.module
 
 internal val partyModule = module {
+	factory { PartiesViewModel(get()) }
 	factory { params -> PartyDetailsViewModel(params.get(), get()) }
 }
--- shared/src/iosMain/kotlin/com/intive/picover/shared/MainViewController.kt
+++ shared/src/iosMain/kotlin/com/intive/picover/shared/MainViewController.kt
@@ -4,16 +4,19 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.ComposeUIViewController
-import cafe.adriel.voyager.navigator.Navigator
-import com.intive.picover.parties.view.PartyDetailsScreen
+import cafe.adriel.voyager.navigator.bottomSheet.BottomSheetNavigator
+import cafe.adriel.voyager.navigator.tab.TabNavigator
+import com.intive.picover.main.navigation.tab.PartiesTab
 
 object MainViewController {
 
+	@OptIn(ExperimentalMaterialApi::class)
 	fun provide() = ComposeUIViewController {
 		Column(
 			modifier = Modifier
@@ -22,7 +25,9 @@ object MainViewController {
 			horizontalAlignment = Alignment.CenterHorizontally,
 			verticalArrangement = Arrangement.spacedBy(10.dp),
 		) {
-			Navigator(PartyDetailsScreen("1QiTXBvKN51vjIjaMfKO"))
+			BottomSheetNavigator {
+				TabNavigator(PartiesTab)
+			}
 		}
 	}
 }
