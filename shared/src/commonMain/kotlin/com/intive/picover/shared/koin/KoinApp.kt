acd6b1b40df4c08e1c141994f1432481a7172748
Marek Macko <marek.macko@intive.com>
2024-01-11 14:33:57 +0100

    Move Profile screen and view model to shared

--- shared/src/commonMain/kotlin/com/intive/picover/shared/koin/KoinApp.kt
+++ shared/src/commonMain/kotlin/com/intive/picover/shared/koin/KoinApp.kt
@@ -1,6 +1,8 @@
 package com.intive.picover.shared.koin
 
+import com.intive.picover.profile.viewmodel.ProfileViewModel
 import com.intive.picover.shared.party.di.partyModule
+import org.koin.dsl.module
 
 expect object KoinApp
 
@@ -10,4 +12,9 @@ internal fun appModules() =
 		platformModule,
 		partyModule,
 		toastModule,
+		profileModule,
 	)
+
+private val profileModule = module {
+	factory { ProfileViewModel(get(), get()) }
+}
