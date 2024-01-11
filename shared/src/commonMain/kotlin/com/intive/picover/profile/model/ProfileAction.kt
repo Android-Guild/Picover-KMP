6977fa6c3496d4b9306d15ca4c4ac289fccf3acb
Marek Macko <marek.macko@intive.com>
2024-01-11 16:48:53 +0100

    Move ProfileAction to common

--- shared/src/commonMain/kotlin/com/intive/picover/profile/model/ProfileAction.kt
+++ shared/src/commonMain/kotlin/com/intive/picover/profile/model/ProfileAction.kt
@@ -9,10 +9,9 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import com.intive.picover.shared.MR
 import dev.icerock.moko.resources.StringResource
 
-sealed class ProfileActionsItem(val text: StringResource, val icon: ImageVector) {
+sealed class ProfileAction(val text: StringResource, val icon: ImageVector) {
 	data object ShowLicenses : ProfileAction(MR.strings.OpenLicenses, Icons.Filled.Copyright)
 	data object Logout : ProfileAction(MR.strings.LogoutButton, Icons.Filled.Logout)
 	data object DeleteAccount : ProfileAction(MR.strings.DeleteAccountButton, Icons.Filled.PersonRemove)
 	data object ShowGitHub : ProfileAction(MR.strings.GithubButton, Icons.Filled.OpenInBrowser)
 }
-typealias ProfileAction = ProfileActionsItem
