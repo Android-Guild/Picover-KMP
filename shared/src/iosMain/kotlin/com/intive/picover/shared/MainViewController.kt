176618cac6cfa9f20d21fcfc194a550903315363
Marek Macko <marek.macko@intive.com>
2024-01-12 15:24:58 +0100

    PicoverImage with coil multiplatform

--- shared/src/iosMain/kotlin/com/intive/picover/shared/MainViewController.kt
+++ shared/src/iosMain/kotlin/com/intive/picover/shared/MainViewController.kt
@@ -1,17 +1,21 @@
 package com.intive.picover.shared
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.ComposeUIViewController
 import cafe.adriel.voyager.navigator.bottomSheet.BottomSheetNavigator
 import cafe.adriel.voyager.navigator.tab.TabNavigator
+import com.intive.picover.common.image.PicoverImage
 import com.intive.picover.main.navigation.tab.PartiesTab
 
 object MainViewController {
@@ -23,8 +27,14 @@ object MainViewController {
 				.fillMaxSize()
 				.background(Color.LightGray),
 			horizontalAlignment = Alignment.CenterHorizontally,
-			verticalArrangement = Arrangement.spacedBy(10.dp),
 		) {
+			PicoverImage(
+				modifier = Modifier
+					.size(120.dp)
+					.clip(CircleShape),
+				imageModel = "https://shorturl.at/cdCFT",
+				contentScale = ContentScale.Crop,
+			)
 			BottomSheetNavigator {
 				TabNavigator(PartiesTab)
 			}
