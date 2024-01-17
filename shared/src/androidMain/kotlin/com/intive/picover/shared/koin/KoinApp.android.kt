3b59c3219776765f16edad8472fae27bf1394a7c
Marek Macko <marek.macko@intive.com>
2024-01-17 16:11:48 +0100

    Migrate WorkManager to koin

--- shared/src/androidMain/kotlin/com/intive/picover/shared/koin/KoinApp.android.kt
+++ shared/src/androidMain/kotlin/com/intive/picover/shared/koin/KoinApp.android.kt
@@ -1,15 +1,28 @@
 package com.intive.picover.shared.koin
 
 import android.app.Application
+import com.intive.picover.shared.common.notification.provider.PicoverNotificationProvider
+import com.intive.picover.shared.photos.work.UploadPhotoWorker
 import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.workmanager.dsl.worker
+import org.koin.androidx.workmanager.koin.workManagerFactory
 import org.koin.core.context.startKoin
+import org.koin.dsl.module
 
 actual object KoinApp {
 
 	fun init(application: Application) {
 		startKoin {
 			androidContext(application)
-			modules(appModules())
+			workManagerFactory()
+			modules(appModules() + androidModules())
 		}
 	}
+
+	private fun androidModules() = listOf(
+		module {
+			factory { PicoverNotificationProvider(get()) }
+			worker { UploadPhotoWorker(get(), get(), get(), get()) }
+		},
+	)
 }
