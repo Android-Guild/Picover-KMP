6c82990053d640620b438aeccc2bb1924f1d78ae
Marek Macko <marek.macko@intive.com>
2024-01-17 16:11:48 +0100

    Drop hilt

--- androidApp/src/main/kotlin/com/intive/picover/main/view/MainActivity.kt
+++ androidApp/src/main/kotlin/com/intive/picover/main/view/MainActivity.kt
@@ -6,9 +6,7 @@ import androidx.activity.compose.setContent
 import cafe.adriel.voyager.navigator.Navigator
 import com.intive.picover.main.theme.PicoverTheme
 import com.intive.picover.shared.main.navigation.view.MainScreen
-import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
 	override fun onCreate(savedInstanceState: Bundle?) {
