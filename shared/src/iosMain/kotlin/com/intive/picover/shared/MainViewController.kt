5126b6307b3244f8fd2fd716db8b9f88b9607380
Marek Macko <marek.macko@intive.com>
2024-01-18 10:57:39 +0100

    Set ComposeApp for iOS

--- shared/src/iosMain/kotlin/com/intive/picover/shared/MainViewController.kt
+++ shared/src/iosMain/kotlin/com/intive/picover/shared/MainViewController.kt
@@ -1,43 +1,21 @@
 package com.intive.picover.shared
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.ComposeUIViewController
-import cafe.adriel.voyager.navigator.bottomSheet.BottomSheetNavigator
-import cafe.adriel.voyager.navigator.tab.TabNavigator
-import com.intive.picover.shared.common.image.PicoverImage
-import com.intive.picover.shared.main.navigation.tab.PartiesTab
 
 object MainViewController {
 
-	@OptIn(ExperimentalMaterialApi::class)
 	fun provide() = ComposeUIViewController {
-		Column(
+		Box(
 			modifier = Modifier
 				.fillMaxSize()
 				.background(Color.LightGray),
-			horizontalAlignment = Alignment.CenterHorizontally,
 		) {
-			PicoverImage(
-				modifier = Modifier
-					.size(120.dp)
-					.clip(CircleShape),
-				imageModel = "https://shorturl.at/cdCFT",
-				contentScale = ContentScale.Crop,
-			)
-			BottomSheetNavigator {
-				TabNavigator(PartiesTab)
-			}
+			ComposeApp()
 		}
 	}
 }
--- shared/src/iosMain/kotlin/com/intive/picover/shared/auth/result/FirebaseAuthContract.ios.kt
+++ shared/src/iosMain/kotlin/com/intive/picover/shared/auth/result/FirebaseAuthContract.ios.kt
@@ -2,10 +2,26 @@ package com.intive.picover.shared.auth.result
 
 import androidx.compose.runtime.Composable
 import com.intive.picover.shared.common.result.ResultContractLauncher
-import platform.Foundation.NSLog
+import dev.gitlive.firebase.Firebase
+import dev.gitlive.firebase.auth.auth
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 @Composable
 actual fun rememberFirebaseAuthResultContract(onResultCode: (Int) -> Unit) =
 	ResultContractLauncher {
-		NSLog("FirebaseAuthResultContract is not yet implemented!")
+		// TODO temporary, just to simplify testing
+		GlobalScope.launch {
+			try {
+				val result = Firebase.auth.signInWithEmailAndPassword("kblvrx00@gmail.com", "1234qwer")
+				if (result.user == null) {
+					onResultCode(4)
+					println("User is null!")
+				} else {
+					println("User signed: ${result.user}")
+				}
+			} catch (exception: Exception) {
+				println("Sign exception $exception")
+			}
+		}
 	}
