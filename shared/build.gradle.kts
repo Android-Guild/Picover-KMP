1191acede1e38b87d6954c89c90a51d541ba5145
Marek Macko <marek.macko@intive.com>
2024-01-24 15:39:20 +0100

    Hide resources

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,3 +1,5 @@
+import dev.icerock.gradle.MRVisibility
+
 plugins {
 	alias(libs.plugins.kotlin.multiplatform)
 	alias(libs.plugins.android.library)
@@ -87,6 +89,7 @@ kotlin {
 
 multiplatformResources {
 	multiplatformResourcesPackage = "com.intive.picover.shared"
+	multiplatformResourcesVisibility = MRVisibility.Internal
 }
 
 android {
