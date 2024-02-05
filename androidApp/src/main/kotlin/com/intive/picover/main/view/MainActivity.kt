acdf42a035f2754631af5d4eb675092a77f7716d
Marek Macko <marek.macko@intive.com>
2024-02-05 16:57:52 +0100

    Simplify status bar styling

--- androidApp/src/main/kotlin/com/intive/picover/main/view/MainActivity.kt
+++ androidApp/src/main/kotlin/com/intive/picover/main/view/MainActivity.kt
@@ -3,11 +3,13 @@ package com.intive.picover.main.view
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import com.intive.picover.shared.ComposeApp
 
 class MainActivity : ComponentActivity() {
 
 	override fun onCreate(savedInstanceState: Bundle?) {
+		enableEdgeToEdge()
 		super.onCreate(savedInstanceState)
 		setContent {
 			ComposeApp()
